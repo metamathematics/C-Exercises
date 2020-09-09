7619bfb84cdcb1d4cff23565e2d4f00235681d40
mr.young <masyoung@hawaii.edu>
2020-09-08 23:06:43 -1000

    Added command line argument capability. Need to add case for when j > 9, and maybe add some error handling.

--- Chapter03/Exercise03_05.c
+++ Chapter03/Exercise03_05.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <string.h>
 #include <stdio.h>
 
 void itob(int, char[], int);
@@ -7,11 +8,17 @@ char toHex(int);
 /* Tests the itob function */
 int main(int arc, char* argv[])
 {
-   char s[1000];
+    char s[1000];
+   
+    if (arc != 3)
+        printf("Usage: ./Exercise03_05 positiveInteger1 positiveInteger2\n");
+    else
+    {
+        itob(toInt(argv[1]), s, toInt(argv[2]));
+        printf("%s\n", s);
+    }
 
-   itob(30, s, 16);
-
-   return 0; 
+    return 0; 
 }
 
 /* itob: converts n to base b as a string s */
@@ -28,7 +35,7 @@ void itob(int n, char s[], int b)
         if (b == 16 && j >= 10 && j <= 15)
             s[i] = toHex(j);
         else
-            s[i] = (j) + '0';
+            s[i] = (j) + '0';/* need case for when j > 9 */
 
         n -= pow(b, k) * j;
         k--;
@@ -63,4 +70,15 @@ char toHex(int digit)
     }
 
     return alpha;
+}
+
+/* toInt: convert str to an int. assumes str is nonneg. */
+int toInt(char *str)
+{
+    int i, num = 0;
+
+    for (i = 0; i < strlen(str); i++)
+        num += (str[i] - '0') * pow(10, strlen(str) - (i + 1));
+
+    return num;
 }
