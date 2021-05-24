1cfdc78ca943c5f7848f3bc566d1a9f978e1db8d
Mr. Young <masyoung@hawaii.edu>
2021-05-23 23:07:57 -1000

    Added comments for 2.3

--- Chapter02/Exercise02_03.c
+++ Chapter02/Exercise02_03.c
@@ -13,9 +13,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-int htoi(char*);
 int hex(char);
+int htoi(char*);
 
+/******************************************************************
+//
+// Name:            main
+//
+// Description:     A hex number is entered as the second
+//                  argument used to start this program.
+//                  The hex is converted to an int, and 
+//                  then printed on the screen.
+//
+// Parameters:      argc (int) - The number of arguments used
+//                               used to call this program.
+//                               Should be 2.
+//
+//                  argv (char**) - An array of the arguments
+//                                  used to call this program.
+//                                  Should be:
+//                                  ./Exercise02_03 someHexNumber
+//
+// Return Value(s): 0
+//
+*******************************************************************/
 int main(int argc, char *argv[])
 {
     int intnum;
@@ -34,6 +55,17 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+/******************************************************************
+//
+// Name:            htoi
+//
+// Description:     Converts a hex number to an integer.
+//
+// Parameters:      hexnum (char*) - The hex number to convert.
+//
+// Return Value(s): the integer equivalent to hexnum.
+//
+*******************************************************************/
 int htoi(char *hexnum)
 {
     char c, s[2];
@@ -60,6 +92,17 @@ int htoi(char *hexnum)
     return intnum;
 }
 
+/******************************************************************
+//
+// Name:            hex
+//
+// Description:     Converts a hex digit to an integer.
+//
+// Parameters:      hexdigit (char) - The hex digit to convert.
+//
+// Return Value(s): the integer equivalent to hexdigit.
+//
+*******************************************************************/
 int hex(char hexdigit)
 {
     int intnum;
