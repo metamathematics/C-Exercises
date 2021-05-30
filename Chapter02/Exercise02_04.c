f5c83e9ce954e28b79b96e6cf2156bc30a245c78
Mr. Young <masyoung@hawaii.edu>
2021-05-29 16:01:13 -1000

    Finished Exercise 2.4. Just need to add comments

--- Chapter02/Exercise02_04.c
+++ Chapter02/Exercise02_04.c
@@ -1,33 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
-void squeeze(char[], char[]);
+void squeeze(char*, char*);
 
-/* prompts the user for 2 strings
- * then deletes all chars in the the first string that match chars in the second
- * displays the result*/
-int main(int arc, char *argv[])
+int main(int argc, char *argv[])
 {
     char s1[1000], s2[1000];
 
-    printf("Enter two strings: ");
-    scanf("%s %s", s1, s2);
-    
+    s1[0] = s2[0] = '\0';
+
+    strcpy(s1, argv[1]);
+    strcpy(s2, argv[2]);
+
     squeeze(s1, s2);
     printf("%s\n", s1);
-
     return 0;
 }
 
-void squeeze(char s1[], char s2[])
+void squeeze(char *s1, char *s2)
 {
-    int i, j, k;
+    int i, j, k, m;
+    char letters[1000], c;
 
-    for (i = 0; s2[i] != '\0'; i++)
+    letters[0] = '\0';
+
+    for (i = m = 0; s2[i] != '\0'; i++)
     {
-        for (j = k = 0; s1[j] != '\0'; j++)
-            if (s1[j] != s2[i])
-                s1[k++] = s1[j];
-        s1[k] = '\0';
-    }    
+        c = s2[i];        
+
+        if (strchr(letters, c) == NULL)
+        {
+            letters[m++] = c;
+            letters[m] ='\0';
+
+            for (j = k = 0; s1[j] != '\0'; j++)
+            {
+                if (s1[j] != c)
+                    s1[k++] = s1[j];
+            }
+            s1[k] = '\0';
+        }
+    }
 }
