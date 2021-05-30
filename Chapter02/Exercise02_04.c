e271e4697b7b414ca0bfe3210b9d0138807c3686
Mr. Young <masyoung@hawaii.edu>
2021-05-29 16:19:17 -1000

    Added comments for Exercise 2.4

--- Chapter02/Exercise02_04.c
+++ Chapter02/Exercise02_04.c
@@ -1,8 +1,39 @@
+/***********************************************
+//
+// File: Exercise02_04.c
+//
+// Author: Mr. Young
+//
+// Description: Executes the squeeze function.
+//
+***********************************************/
+
 #include <stdio.h>
 #include <string.h>
 
 void squeeze(char*, char*);
 
+/**************************************************************************
+//
+// Name:            main
+//
+// Description:     Compares two strings (s1 and s2). All letters in
+//                  s1 that are also in s2, are deleted from s1. Then
+//                  s1 is printed.
+//
+// Parameters:      argc (int) - The number of arguments used to call
+//                               this program. This number should be 3.
+//
+//                  argv (char**) - An array of all the the arguments
+//                                  used to call this function. There
+//                                  should be three arguments:
+//                                  1) The executable name
+//                                  2) s1
+//                                  3) s2
+//
+// Return Value(s): 0
+//
+**************************************************************************/
 int main(int argc, char *argv[])
 {
     char s1[1000], s2[1000];
@@ -17,6 +48,20 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+/**************************************************************************
+//
+// Name:            squeeze
+//
+// Description:     Compares two strings (s1 and s2). All letters in
+//                  s1 that are also in s2, are deleted from s1.
+//
+// Parameters:      s1 (char*) - The string to delete letters from.
+//
+//                  s2 (char*) - The string to compare to s2.
+//
+// Return Value(s): 0
+//
+**************************************************************************/
 void squeeze(char *s1, char *s2)
 {
     int i, j, k, m;
@@ -38,6 +83,7 @@ void squeeze(char *s1, char *s2)
                 if (s1[j] != c)
                     s1[k++] = s1[j];
             }
+
             s1[k] = '\0';
         }
     }
